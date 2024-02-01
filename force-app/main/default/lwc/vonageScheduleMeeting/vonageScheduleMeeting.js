99b1341ad43fe8c8fa0640d2073f7d71a658a15e
Adam Redlinski <adam.r@bsky.co.za>
2024-02-01 15:47:30 +0100

    added object for representing meetings, added flow and class to invoke request to schedule meeting, updated layouts and profiles

--- force-app/main/default/lwc/vonageScheduleMeeting/vonageScheduleMeeting.js
+++ force-app/main/default/lwc/vonageScheduleMeeting/vonageScheduleMeeting.js
@@ -1,5 +1,18 @@
 import { LightningElement, api } from 'lwc';
 
 export default class VonageScheduleMeeting extends LightningElement {
-    @api guestUrl;
+    @api recordId;
+    guestUrl;
+    @api meetingName;
+    @api expirationDate;
+
+    connectedCallback() {
+        // Get the current URL
+        const currentUrl = window.location.origin;
+        console.log('test', currentUrl + 'lightning/r/VonageCall__c/a007R000014VqBMQA0/view');
+        // console.log('test  window.location.origin',  window.location.origin);
+
+        // Set the guestUrl to the current URL
+        this.guestUrl ='https://youriteams3-dev-ed.develop.lightning.force.com/lightning/r/VonageCall__c/a007R000014VqBMQA0/view';
+    }
 }
