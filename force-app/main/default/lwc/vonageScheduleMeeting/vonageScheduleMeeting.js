8a938d7b9829ff8f4502ea35bce521a03b0e2a43
Adam Redlinski <adam.r@bsky.co.za>
2024-02-01 11:24:05 +0100

    added a quick action for case to schedule meeting

--- force-app/main/default/lwc/vonageScheduleMeeting/vonageScheduleMeeting.js
+++ force-app/main/default/lwc/vonageScheduleMeeting/vonageScheduleMeeting.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class VonageScheduleMeeting extends LightningElement {}
+export default class VonageScheduleMeeting extends LightningElement {
+    @api guestUrl;
+}
